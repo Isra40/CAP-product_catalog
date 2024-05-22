2a1475184ec6dfb9432602a8c15d81218c4a2abd
Isra40 <guzman.israel@icloud.com>
2024-05-22 09:41:33 +0000

    Bloque VI - CAP CDL y CQL. 8. Consultas CQL Embebidas- 8.3 On READ - Filtros

--- srv/orders.js
+++ srv/orders.js
@@ -5,6 +5,12 @@ module.exports = (srv) => {
 
     //*********** READ **********/
     srv.on("READ", "GetOrders", async (req) => {
+        
+//      Si se solicita un cliente        
+        if (req.data.ClientEmail !== undefined) {
+            return await SELECT.from`com.training.Orders`
+            .where `ClientEmail = ${req.data.ClientEmail}`;
+        }
         return await SELECT.from(Orders);
     } );
 
